349be3e1fb234b75a8d8aba9fc5015e67182c50c
sherazAhmed-ipynb <sherazahmed3912@gmail.com>
2025-04-18 21:52:39 +0500

    fixed

--- steamLit/testingApp/testApp.py
+++ steamLit/testingApp/testApp.py
@@ -44,6 +44,4 @@ st.sidebar.write(f"Hello {name}")
 # adding a file uploader to the app
 upload_file = st.sidebar.file_uploader("Choose a CSV file", type="csv")
 
-# adding a drop down list to the app
-# option = st.selectbox(
 
